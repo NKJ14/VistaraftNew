e894719d79e79fe124a8e165f17999f554439377
NKJ14 <nishanthere1402@gmail.com>
2025-02-08 01:02:30 +0530

    Init Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+function App() {
+
+  return (
+    <div className="w-screen max-w-full overflow-hidden">
+      <Header/>
+      <Hero/>
+      <Hero/>
+      <Hero/>
+      <Hero/>
+      <Hero/>
+      
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
